0913b7dd05b4f3bf687cd36a8972543e7b484599
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2022-12-05 18:54:59 -0600

    Day 1 f, Day 2 s

--- 2022/Day_1/Day_1.ts
+++ 2022/Day_1/Day_1.ts
@@ -1,13 +1,21 @@
 function wrapping(gifts: string[]): string[] {
-    return []
+	let wraped:string[] = []
+
+	for(let i = 0; i < gifts.length; i++){
+		let len:number = gifts[i].length
+		wraped.push("*".repeat(len + 2)+"\\n*"+gifts[i]+"*\\n"+"*".repeat(len + 2))
+	}
+
+	return wraped
 }
 
 const gifts = ['cat', 'game', 'socks']
+const expected = ["*****\\n*cat*\\n*****", "******\\n*game*\\n******", "*******\\n*socks*\\n*******"]
 const wrapped = wrapping(gifts)
 
 console.log(wrapped)
 /* [
-  "*****\\n*cat*\\n*****",
-  "******\\n*game*\\n******",
-  "*******\\n*socks*\\n*******"
+	"*****\\n*cat*\\n*****",
+	"******\\n*game*\\n******",
+	"*******\\n*socks*\\n*******"
 ] */
--- 2022/Day_2/Day_2.ts
+++ 2022/Day_2/Day_2.ts
@@ -1,5 +1,16 @@
-function countHours(year: number, holidays: string[]): number {
-    return 0
+
+function countHours(year: number, holidays: string[]):Number {
+    let hours:number = 0
+    for(let i = 0; i < holidays.length; i++){
+        let holiday:string[] = holidays[2].split("/")
+        let date:string = `${year}-${holiday[0]}-${holiday[1]}` 
+        let day:Date = new Date(date) 
+
+        if(day.getDay() !== 0 && day.getDay() !== 6){
+            hours += 2;
+        }
+    } 
+    return hours
 }
 
 const year = 2022
@@ -9,4 +20,4 @@ const holidays = ['01/06', '04/01', '12/25'] // format MM/DD
 // 04/01 is April 1, Friday. Count.
 // 12/25 is December 25, Sunday. Do not count.
 
-countHours(year, holidays) // 2 days -> 4 extra hours in the year
+console.log(countHours(year, holidays)) // 2 days -> 4 extra hours in the year
